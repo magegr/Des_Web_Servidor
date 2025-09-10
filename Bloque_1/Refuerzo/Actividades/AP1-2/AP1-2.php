534712649ce83cbfcfa6e8227cb1a79fb75e46f7
Maria Genis <magegr@alumnatflorida.es>
2025-09-10 19:06:16 +0200

    Exercise 2 completed

--- Bloque_1/Refuerzo/Actividades/AP1-2/AP1-2.php
+++ Bloque_1/Refuerzo/Actividades/AP1-2/AP1-2.php
@@ -1,3 +1,14 @@
 <?php
+$datos = [];
+$datos= [$_GET['hola'] ?? null, $_GET['clave'] ?? null, $_GET['clave2'] ?? null];//si no existe se toma como nulo
 
+foreach ($datos as $key => $value) { //el orden importa
+    if (is_null($value)){
+        echo 'No se ha recibido ningún dato o es un valor nulo  <br>';
+    }elseif (is_numeric($value)){
+        echo 'Se ha recibido un número <br>';
+    }else{
+        echo ' Se ha recibido una cadena de caracteres<br>';
+    }
+}
 ?>
