b35c362a7d19e6a2663bf1f4eb572af3ab5f3232
Maria Genis <magegr@alumnatflorida.es>
2025-09-10 19:05:14 +0200

    Exercise  1 completed

--- Bloque_1/Refuerzo/Actividades/AP1-1/AP1-1.php
+++ Bloque_1/Refuerzo/Actividades/AP1-1/AP1-1.php
@@ -1,1 +1,8 @@
 <?php
+$datos = [];
+$datos= [$_GET['hola'] , $_GET['clave'] , $_GET['clave2']];
+
+foreach ($datos as $key => $value) {
+    echo 'Se ha recibido el valor '. $value.' para la clave'. $key.'<br>';
+}
+?>
