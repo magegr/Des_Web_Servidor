e07d475bf8a61248af87c5a30012af41e9a5e2f7
Maria Genis <magegr@alumnatflorida.es>
2025-09-11 17:53:04 +0200

    Exercise 3 completed

--- Bloque_1/Refuerzo/Actividades/AP1-3/AP1-3.php
+++ Bloque_1/Refuerzo/Actividades/AP1-3/AP1-3.php
@@ -1,1 +1,37 @@
 <?php
+$numeros=[];
+$numeros=[
+    1 => "primero",
+    3 => "segundo",
+    5 => "tercero",
+    7 => "cuarto",
+    9 => "quinto",
+    11 => "sexto",
+];
+$posicion=0;
+$suma=0;
+foreach ($numeros as $numero => $nombre) {
+    if ($posicion % 2 == 0) {
+        echo 'Estas en una posición par';
+        $impar=false;
+        $par=true;
+    }else {
+        echo 'Estas en una posición impar';
+        $impar = true;
+        $par = false;
+    }
+    $suma=$suma+$numero;
+    echo '<br>'.'La suma es '.$suma.'<br>';
+
+    if ($suma > 5){
+        echo 'El valor de la suma es mayor que 5'.'<br>';
+    }
+    if ($suma > 10){
+        echo 'El valor de la suma es mayor que 10'.'<br>';
+    }
+    if ($suma > 20){
+        echo 'El valor de la suma es mayor que 20'.'<br>';
+    }
+    $posicion++;
+}
+?>
