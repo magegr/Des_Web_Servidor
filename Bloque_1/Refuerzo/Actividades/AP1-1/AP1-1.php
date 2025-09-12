b041bc31a8dccee46a6f324ad14bdea89aa375bd
Maria Genis <magegr@alumnatflorida.es>
2025-09-12 08:46:30 +0200

    Exercise 1 fixed

--- Bloque_1/Refuerzo/Actividades/AP1-1/AP1-1.php
+++ Bloque_1/Refuerzo/Actividades/AP1-1/AP1-1.php
@@ -1,6 +1,9 @@
 <?php
 $datos = [];
-$datos= [$_GET['hola'] , $_GET['clave'] , $_GET['clave2']];
+if (isset($_GET)){ //comprobar si existe primero
+    $datos= $_GET;//cambiar , no hace falta poner todas las cosas , no es practico
+}
+
 
 foreach ($datos as $key => $value) {
     echo 'Se ha recibido el valor '. $value.' para la clave'. $key.'<br>';
