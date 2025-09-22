4bd1796a92992f9c3face27745338efe8b3af5b5
Maria Genis <magegr@alumnatflorida.es>
2025-09-22 13:17:16 +0200

    exercise 1 puebas 2 error con la function protected

--- Bloque_1/POO/AP2-1_index.php
+++ Bloque_1/POO/AP2-1_index.php
@@ -1,3 +1,7 @@
 <?php
 require_once ('AP2-1.php');
+require_once ('AP2-1_1.php');
+require_once ('AP2-1_2.php');
 $coche1= new cocheElectricoF1('ford','mondeo',160,200,2,100);
+$coche1->mostrarEstado();
+$coche1->consumirCombustible(10);
