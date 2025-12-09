5cff4fdeb9b55b7a4c07e38404dbb3d3c056ec54
Maria Genis <magegr@alumnatflorida.es>
2025-12-09 10:02:01 +0100

    terminado el AP7_3

---
+++ Bloque_3/Sinfony/Actividades/AP7_3/src/Form/ProductType.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Product;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\NumberType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class ProductType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name', TextType::class, ['label' => 'Nombre'])
+            ->add('price', NumberType::class, ['label' => 'Precio'])
+            ->add('stock', NumberType::class, ['label' => 'Stock'])
+            ->add('createdAt', DateType::class, ['label' => 'Fecha de creacion']);
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Product::class,
+        ]);
+    }
+}
